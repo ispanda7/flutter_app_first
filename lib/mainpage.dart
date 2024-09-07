d4512b3a0354f54bb66fb6510f192b408a3efd77
iAeeD <ispanda7@iAeeDs-MacBook-Air.local>
2024-09-07 15:15:14 +0700

    first commit

---
+++ lib/mainpage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: const Center(
+        child: Text('Home dev 2'),
+      ),
+    );
+  }
+}
